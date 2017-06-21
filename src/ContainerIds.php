b42d2a83a38a840935201e4fd63bd829b1459cba
slimek <slime573@gmail.com>
2017-06-21 16:25:04 +0800

    Add Slim\Middleware\RouteLogger

---
+++ src/ContainerIds.php
@@ -0,0 +1,8 @@
+<?php
+namespace Praline;
+
+// 在使用 Dependencies Injection 時，Praline 元件向 container 查詢元件所使用的名稱
+class ContainerIds
+{
+    public const LOGGER = 'logger';
+}
---
+++ src/Slim/Middleware/RouteLogger.php
@@ -0,0 +1,55 @@
+<?php
+namespace Praline\Slim\Middleware;
+
+// Composer
+use Psr\Log\LoggerInterface;
+use Slim\Http\
+{
+    Request, Response, Uri
+};
+
+// Parline
+use Praline\ContainerIds;
+
+// Slim Framework Middleware - Route 用 Logger
+// 1. API 呼叫與回應的歷程記錄
+// 2. 攔截異常，輸出錯誤訊息
+// 通常會是最外層的 middleware
+class RouteLogger
+{
+    /** @var  LoggerInterface */
+    private $logger;
+
+    public function __construct($container)
+    {
+        $this->logger = $container[ContainerIds::LOGGER];
+    }
+
+    public function __invoke(Request $request, Response $response, $next)
+    {
+        /** @var Uri $uri */
+        $uri = $request->getUri();
+
+        $pathAndQuery = empty($uri->getQuery())
+                      ? $uri->getPath()
+                      : $uri->getPath() . '?' . $uri->getQuery();
+
+        $this->logger->info($pathAndQuery);
+
+        try
+        {
+            $response = $next($request, $response);
+
+        } catch (\Throwable $t) {
+
+            $this->logger->error($t->getMessage() . PHP_EOL . $t->getTraceAsString());
+
+            $response = $response->withStatus(500)
+                                 ->write($t->getMessage());
+        }
+
+        $this->logger->info($response->getStatusCode());
+
+        return $response;
+    }
+}
